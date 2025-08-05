7dae1e3e9cd57d498b4b18b7e42e7c86b2860542
Immanuel W <l0calh34rtz@gmail.com>
2025-08-05 10:17:25 +0700

    feat: key

--- src/Results.jsx
+++ src/Results.jsx
@@ -12,11 +12,11 @@ const Results = ({ pets = [] }) => {
               animal={pet.animal}
               name={pet.name}
               breed={pet.breed}
-              key={pet.id}
+              key={pet.id} // ini adalah component yang unik / distinct yg diberikan dari FE
               images={pet.images}
               city={pet.city}
               state={pet.state}
-              id={pet.id}
+              id={pet.id} // penanda uniqude Identifier (ID) si Pet dari API
             />
           );
         })
