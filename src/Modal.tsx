438ddd370d84258ae8f6546272dbfcbe20d0d1f1
Immanuel W <l0calh34rtz@gmail.com>
2025-08-06 10:14:07 +0700

    feat: add ts config

--- src/Modal.tsx
+++ src/Modal.tsx
@@ -1,9 +1,9 @@
-import { useEffect, useRef } from "react";
+import { MutableRefObject, ReactElement, useEffect, useRef } from "react";
 import { createPortal } from "react-dom";
 
-const Modal = ({ children }) => {
+const Modal = ({ children }: { children: ReactElement }) => {
   // elRef memiliki default value null
-  const elRef = useRef(null);
+  const elRef: MutableRefObject<HTMLDivElement | null> = useRef(null);
 
   // step 1 adalah mengecek elRef.current.
   // apakah tidak ada elRef.current? jika tidak ada, maka assign task untuk create element div pada elRef.current
@@ -15,13 +15,24 @@ const Modal = ({ children }) => {
   useEffect(() => {
     // kita menambahkan instance/shortcut dari element "modal" ke variable "modalRoot".
     const modalRoot = document.getElementById("modal");
+
+    if (!modalRoot || !elRef.current) {
+      return;
+    }
     // kita menambahkan child ke dalam element "modal", dalam hal ini adalah elRef.current.
     modalRoot.appendChild(elRef.current);
 
     // step 3
     // flushing method --> method yang disediakan oleh useEffect yang dipanggil ketika modalnya ditutup.
-    return () => modalRoot.removeChild(elRef.current);
+    return () => {
+      // ngecek apabila elRef.current ada isi atau bernilai true, maka silahkan hapus dia dari modalRoot. Kalo tidak ada/false, flushnya berhenti.
+      if (elRef.current) {
+        modalRoot.removeChild(elRef.current);
+      }
+    };
   }, []);
+
+  // alasan kita menambahkan if ke dalam useEffect dikarenakan elRef (useRef dalam hal ini) dapat memiliki nilai null. Oleh karena nilai null tersebut dia perlu dicek disetiap tempat si elRef dipakai/digunakan.
 
   // children adalah tempat dimana kita memasukan form, label, dsbnya yang merupakan isi dari modal kita. Elref.current dalam createPortal ini merupakan wrappernya.
   return createPortal(<div>{children}</div>, elRef.current);
